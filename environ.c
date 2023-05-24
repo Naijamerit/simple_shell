bf13f673ea3ea7170dab222fac55f13879614711
Victor Ejiogu <victor.ejiogu@ust.edu.ng>
2023-05-24 10:03:33 +0100

    everon

--- environ.c
+++ environ.c
@@ -1,92 +1,106 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
 #include "shell.h"
 
-/**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
-int _myenv(info_t *info)
-{
-	print_list_str(info->env);
-	return (0);
-}
+#define MAX_LINE 80
+
+extern char **environ;
 
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
+ * search_path - search for a command in the PATH
+ * @cmd: the command to search for
  *
- * Return: the value
+ * Return: If the command is found, return the full path. Otherwise, return NULL.
  */
-char *_getenv(info_t *info, const char *name)
+char *search_path(char *cmd)
 {
-	list_t *node = info->env;
-	char *p;
+	char *path = getenv("PATH");
+	char *path_copy = strdup(path);
+	char *dir = strtok(path_copy, ":");
+	char *full_path = malloc(MAX_LINE);
 
-	while (node)
+	while (dir != NULL)
 	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
+	strcpy(full_path, dir);
+	strcat(full_path, "/");
+	strncat(full_path, cmd, MAX_LINE - strlen(full_path) - 1);
+	if (access(full_path, X_OK) == 0)
+	{
+	free(path_copy);
+	return (full_path);
 	}
+	dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	free(full_path);
 	return (NULL);
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * main - simple shell
+ *
+ * Return: Always 0 (Success)
  */
-int _mysetenv(info_t *info)
+int main(void)
 {
-	if (info->argc != 3)
+	char *args[MAX_LINE / 2 + 1];
+	int should_run = 1;
+	char line[MAX_LINE];
+	pid_t pid;
+	int status;
+
+	while (should_run)
 	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
+	write(STDOUT_FILENO, "osh> ", 5);
+	fgets(line, MAX_LINE, stdin);
+	line[strcspn(line, "\n")] = '\0';
+
+	int i = 0;
+	args[i] = strtok(line, " ");
+	while (args[i] != NULL)
+	{
+	i++;
+	rgs[i] = strtok(NULL, " ");
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+
+	if (strcmp(args[0], "exit") == 0)
+	{
+	should_run = 0;
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		for (i = 0; environ[i] != NULL; i++)
+	{
+		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+	write(STDOUT_FILENO, "\n", 1);
+	}
+	}
+	else
+	{
+	char *cmd_path = search_path(args[0]);
+	if (cmd_path != NULL)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+		execv(cmd_path, args);
+		}
+		else
+		{
+		wait(&status);
+		}
+		free(cmd_path);
+		}
+		else
+		{
+		write(STDERR_FILENO, "Command not found\n", 17);
+			}
+		}
+	}
+
 		return (0);
-	return (1);
 }
 
-/**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
-int _myunsetenv(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
-}
